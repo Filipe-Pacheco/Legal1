bedcb29876b89d4f544348ec33cffe6cc9bfd324
Filipe Pacheco <52587073+Filipe-Pacheco@users.noreply.github.com>
2021-03-05 18:13:45 -0300

    Add files via upload

---
+++ script-paginas.js
@@ -0,0 +1,42 @@
+
+//menuhome
+document.getElementById("menu-id-4bd31506d9e045fc8c0aba6da3ebed2d").href = "menuhome.html";
+
+//menucontatos
+document.getElementById("menu-id-babe357280f24735842ed50a4b98e453").href = "menucontatos.html";
+
+//menuagenda
+document.getElementById("menu-id-5fedb1cdae424d4fb483485d6580e53a").href = "menuagenda.html";
+
+//menupublicationmanagement
+document.getElementById("menu-id-b58e1ddbf20143dc8c15db088eefd585").href = "menupublicationmanagement.html";
+
+//menuprocessos
+document.getElementById("menu-id-8d763e8a04af4af4a5a01368d62bb294").href = "menuprocessos.html";
+
+//menuservicos
+document.getElementById("menu-id-6081f2b8530049558505caf79f09dd00").href = "menuservicos.html";
+
+//menucontratos
+document.getElementById("menu-id-dee3ce39d7cf44f7b882b9bd8204407f").href = "menucontratos.html";
+
+//menufinanceiro
+document.getElementById("menu-id-5da150c9b169460d9e353b32ec16083e").href = "menufinanceiro.html";
+
+//menufaturamento
+document.getElementById("menu-id-643ce79d21a24456b018d42b8bf91b00").href = "menufaturamento.html";
+
+//menuboletos
+document.getElementById("menu-id-e89977b73b774ec5a261469034dced95").href = "menuboletos.html";
+
+//menuged
+document.getElementById("menu-id-779831c8bc8e4ad78938fe05a0265491").href = "menuged.html";
+
+//menuacervojuridico
+document.getElementById("menu-id-37f6dd1c86e8463ca7eee256f11f292c").href = "menuacervojuridico.html";
+
+//menutimesheet
+document.getElementById("menu-id-e4caab1a2fff450b9676d49d47b20853").href = "menutimesheet.html";
+
+//menuworkflow
+document.getElementById("menu-id-f0a1e98f0cc14847aa56cf93973155d8").href = "menuworkflow.html";
